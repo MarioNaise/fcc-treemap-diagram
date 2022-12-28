7bb46e806b282b0f5ae2ab1631daf0ec1528f981
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-12-28 20:52:06 +0000

    added tooltip & text (text wip)

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const kickstarterDataURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json",
 movieDataURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json",
 gameDataURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json",
-w = 600,
-h = 400,
+w = 800,
+h = 600,
 p = 2;
 
 const svg = d3.select("#canvas")
   .attr("width", w)
   .attr("height", h)
 
+
+const tooltip = d3
+  .select("body")
+  .append("div")
+  .attr("id", "tooltip");
 
 
 d3.json(gameDataURL)
@@ -18,10 +23,9 @@ d3.json(gameDataURL)
   console.log(data)
 
   const categories = data.children.map(d => d.name)
-  const dataset = data.children.map(d => d.children).flat()
 
   // give the data to this cluster layout
-  const root = d3.hierarchy(data.children[2]).sum(d=>d.value)
+  const root = d3.hierarchy(data).sum(d=>d.value)
 
   // create treemap
   d3.treemap()
@@ -44,9 +48,41 @@ d3.json(gameDataURL)
       .attr("y", d => d.y0)
       .attr("width", d => d.x1 - d.x0)
       .attr("height", d => d.y1 - d.y0)
-      .style("stroke", "black")
-      .style("fill", (d) => colorScale(d.data.category))
+      .style("stroke", d => colorScale(d.data.category))
+      .style("fill", d => colorScale(d.data.category))
       .attr("class", "tile")
+      .attr("data-name", d => d.data.name)
+      .attr("data-category", d => d.data.category)
+      .attr("data-value", d => d.data.value)
+      .on("mouseover", (e) => {
+                  tooltip
+                    .style("display", "block")
+                    .style("left", `${e.pageX + 10}px`)
+                    .style("top", `${e.pageY + 10}px`)
+                    .html(
+                    `
+                      <p>Name: ${e.target.attributes["data-name"].value}</p>
+                      <p>Category: ${e.target.attributes["data-category"].value}</p>
+                      <p>Value: ${e.target.attributes["data-value"].value}</p>
+                    `
+                    )
+                    .attr("data-value", e.target.attributes["data-value"].value)
+                })
+                .on("mouseout", () => {
+                  tooltip.style("display", "none")
+                })
+  
+  svg
+    .selectAll("vals")
+    .data(root.leaves())
+    .enter()
+    .append("text")
+      .attr("x", d => d.x0 + 5)
+      .attr("y", d => d.y0 + 10)
+      .text(d => d.data.name)
+      .attr("font-size", "11px")
+      .attr("fill", "white")
+
 })
 
 /*
